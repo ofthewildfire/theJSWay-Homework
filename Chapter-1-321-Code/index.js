c515b8670f77cc0f7df903b42026b280d2aad975
ofthewildfire <lissajace@gmail.com>
2023-05-17 14:32:32 +0200

    finished chp 1

---
+++ Chapter-1-321-Code/index.js
@@ -0,0 +1,31 @@
+/* 
+Presentation
+Write a program that displays your name and age. Here's the result for mine.
+*/
+
+const name = "Kaycee";
+const age = 28;
+// console.log(name);
+// console.log(age);
+/* 
+Minimalistic calculator
+Write a program that displays the results of adding, subtracting, multiplying and dividing 6 by 3.
+*/
+
+// Add 6/3
+// console.log(6 + 3);
+// Subtract 6/3
+// console.log(6 - 3);
+// Multiply 6/3
+// console.log(6 * 3);
+// Divide 6/3
+// console.log(6 / 3);
+
+/* 
+Values prediction
+Observe the following program and try to predict the values it displays.
+*/
+
+// console.log(4 + 5); 9
+// console.log("4 + 5"); 4+5
+// console.log("4" + "5"); 45
