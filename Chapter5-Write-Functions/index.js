5df2a8b6e50b93f12b638b3704bf492bb1548ed1
ofthewildfire <lissajace@gmail.com>
2023-05-24 12:19:11 +0200

    finished first task in chapter 5

--- Chapter5-Write-Functions/index.js
+++ Chapter5-Write-Functions/index.js
@@ -1,2 +1,24 @@
 const r = document.querySelector("h2");
 r.innerText = "Not what is in the HTML";
+
+/* 
+Improved hello
+Complete the following program so that it asks the user for his first and last names, then show the result of the sayHello() function.
+
+
+*/
+// Say hello to the user
+function sayHello(firstName, lastName) {
+	const message = `Hello, ${firstName} ${lastName}!`;
+	return message;
+}
+
+// let firstName = prompt("What is your first name: ");
+// let lastName = prompt("What is your last name: ");
+
+let greeting = sayHello(firstName, lastName);
+
+console.log(greeting);
+
+// TODO: ask user for first and last name
+// TODO: call sayHello() and show its result
