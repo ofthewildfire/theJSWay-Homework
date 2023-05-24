23318a8a89fb14c4146d6105f578ea52759cd31e
ofthewildfire <lissajace@gmail.com>
2023-05-24 13:17:11 +0200

    added

--- Chapter5-Write-Functions/index.js
+++ Chapter5-Write-Functions/index.js
@@ -1,5 +1,67 @@
-const r = document.querySelector("h2");
-r.innerText = "Not what is in the HTML";
+/* 
+Minimum of two numbers
+Let's pretend the JavaScript Math.min() function doesn’t exist. Complete the following program so that the min() function returns the minimum of its two received numbers.
+
+// TODO: write the min() function
+
+*/
+
+function min(a, b) {
+	if (a < b) {
+		return a;
+	} else if (b < a) {
+		return b;
+	} else if (a === b) {
+		return a;
+	}
+}
+
+// console.log(min(4.5, 5)); // Must show 4.5
+// console.log(min(19, 9)); // Must show 9
+// console.log(min(1, 1)); // Must show 1
+
+console.log(min(45, 51)); // Must show 45
+console.log(min(2, 1)); // Must show 1
+console.log(min(0, 1)); // Must show 0
+
+/* 
+Number squaring
+Complete the following program so that the square1() and square2() functions work properly.
+
+When it's done, update the program so that it shows the square of every number between 0 and 10.
+*/
+
+function sq1() {
+	for (let i = 0; i <= 10; i++) {
+		console.log(`The sqrt of ${i} is: ${i * i * i} `);
+	}
+}
+
+// console.log(sq1());
+
+// Square the given number x
+function square1(x) {
+	let exponent = x;
+	let base = x;
+
+	return base * exponent;
+
+	// TODO: complete the function code
+}
+
+// Square the given number x
+const square2 = (x) => x * x;
+// TODO: complete the function code
+
+// console.log(square1(0)); // Must show 0
+// console.log(square1(2)); // Must show 4
+// console.log(square1(5)); // Must show 25
+//
+// console.log("Top is sq1 - bottom is sq2");
+//
+// console.log(square2(0)); // Must show 0
+// console.log(square2(2)); // Must show 4
+// console.log(square2(5)); // Must show 25
 
 /* 
 Improved hello
@@ -8,17 +70,17 @@ Complete the following program so that it asks the user for his first and last n
 
 */
 // Say hello to the user
-function sayHello(firstName, lastName) {
-	const message = `Hello, ${firstName} ${lastName}!`;
-	return message;
-}
+// function sayHello(firstName, lastName) {
+// 	const message = `Hello, ${firstName} ${lastName}!`;
+// 	return message;
+// }
 
 // let firstName = prompt("What is your first name: ");
 // let lastName = prompt("What is your last name: ");
 
-let greeting = sayHello(firstName, lastName);
+// let greeting = sayHello(firstName, lastName);
 
-console.log(greeting);
+// console.log(greeting);
 
 // TODO: ask user for first and last name
 // TODO: call sayHello() and show its result
