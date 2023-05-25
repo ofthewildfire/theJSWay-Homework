69b8ed821b70847517b3863df1f341aecf04f6c1
ofthewildfire <lissajace@gmail.com>
2023-05-25 20:36:45 +0200

    todays hw

---
+++ JSinfo.net-Array-Methods/app.js
@@ -0,0 +1,5 @@
+// arr.splice(start[, deleteCount, elem1, ..., elemN])
+
+const arr = ["Pig", "Butchering", "Scammers", "From", "China"];
+
+//Concat : The method arr.concat creates a new array that includes values from other arrays and additional items.
