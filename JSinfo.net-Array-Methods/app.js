dc3a311e7a9104888520099e174afe5f1c94ed9c
ofthewildfire <lissajace@gmail.com>
2023-05-25 20:53:05 +0200

    added concat

--- JSinfo.net-Array-Methods/app.js
+++ JSinfo.net-Array-Methods/app.js
@@ -3,3 +3,8 @@
 const arr = ["Pig", "Butchering", "Scammers", "From", "China"];
 
 //Concat : The method arr.concat creates a new array that includes values from other arrays and additional items.
+
+const arr2 = ["Summer", "Winter", "Emma Swan", "Evil Queen", "Belle"];
+
+const newArr = arr.concat(arr2);
+console.log(newArr);
