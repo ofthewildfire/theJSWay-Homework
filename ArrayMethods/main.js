f35d785921475b84a6e161fd04708385d166532d
ofthewildfire <lissajace@gmail.com>
2023-07-10 21:39:36 +0200

    added another version

--- ArrayMethods/main.js
+++ ArrayMethods/main.js
@@ -108,3 +108,8 @@ let multipleOfIndex = (a) => a.filter((n, i) => n % i === 0);
 function arrayPlusArray(arr1, arr2) {
 	return arr1.concat(arr2).reduce((a, c) => a + c, 0);
 }
+
+function arrayPlusArray(arr1, arr2) {
+	let arr = [...arr1, ...arr2];
+	return arr.reduce((a, b) => a + b);
+}
